da9a100e8c327fa4317f5c3b1ef6fd57a14223d7
andrew28092002 <andreiastanaev2002@gmail.com>
2023-03-06 22:20:27 +0500

    update item-details

--- src/app/components/item-details/item-details.component.ts
+++ src/app/components/item-details/item-details.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { IItem } from 'src/app/models/item';
+import { ItemService } from 'src/app/shared/item.service';
 
 @Component({
   selector: 'app-item-details',
   templateUrl: './item-details.component.html',
-  styleUrls: ['./item-details.component.scss']
+  styleUrls: ['./item-details.component.scss'],
+  providers: [ItemService],
 })
-export class ItemDetailsComponent {
+export class ItemDetailsComponent implements OnInit {
+  item: IItem;
 
+  constructor(
+    private route: ActivatedRoute,
+    private ItemService: ItemService
+  ) {}
+
+  ngOnInit(): void {
+    this.route.params.subscribe((params) => {
+      this.ItemService.getById(params['id']).subscribe((item) => {
+        this.item = item;
+      });
+    });
+  }
 }
